594c162588ea8dc4eb38b1ab96f4496b0815f8e5
Srinjoy-Chatterjee <87896364+Srinjoy-Chatterjee@users.noreply.github.com>
2023-11-16 15:57:45 +0530

    update

--- app.py
+++ app.py
@@ -1,32 +1,9 @@
 import streamlit as st
-import subprocess
-
-def run_notebook(notebook_path):
-    command = f"jupyter nbconvert --to notebook --execute {notebook_path} --output-dir=output"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    process.communicate()
+import pandas as pd
 
 st.title("Sentiment Analysis")
-company = st.text_input("Enter company name:")
-with open("company.txt","w") as file:
-    strs = file.write(company)
-if st.button("Analyze"):
-    # Run the notebook
-    run_notebook("main.ipynb")
-    with open("score.txt","r") as file:
-        strs = (file.read()).split(" ")
-        prev = float(strs[0])
-        base = float(strs[1])
-        st.info(f"Previous Score : {prev}")
-        st.info(f"Current Score : {base}")
-        if(prev==base) :
-            st.warning(f"Delta Score : {-prev+base}")
-        if(prev>base) :
-            st.error(f"Delta Score : {-prev+base}")
-        if(base>prev) :
-            st.success(f"Delta Score : {-prev+base}")
-        if(base!=prev) :
-            st.success(f"Mail Send Successfully!!")
-
-            
-            
+nav = st.sidebar.radio("Navigation",["Home","Add"])
+if nav=="Home":
+    st.write("Home")
+if nav=="Add":
+    st.write("Add")
